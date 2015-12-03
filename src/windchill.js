8d08c235044bddf8864cf9fdae187f2d287ab458
Nic Aitch <nic@nicinabox.com>
2015-12-03 14:02:46 -0600

    Add support for SI formula

--- src/windchill.js
+++ src/windchill.js
@@ -1,4 +1,27 @@
-export default (temp, speed) => {
-  var rawTemp = 35.74 + (0.6215 * temp) - 35.75 * Math.pow(speed, 0.16) + (0.4275 * temp) * Math.pow(speed, 0.16)
-  return Math.round(rawTemp, 0);
+const SI_TEMP_MAX = 10
+const SI_SPEED_MIN = 4.8
+const US_TEMP_MAX = 50
+const US_SPEED_MIN = 3
+
+var round = (n) => Math.round(n, 0)
+
+var si = (temp, speed) => {
+  if (temp > SI_TEMP_MAX || speed < SI_SPEED_MIN) return
+  return round(13.12 + (0.6215 * temp) - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16))
+}
+
+var us = (temp, speed) => {
+  if (temp > US_TEMP_MAX || speed < US_SPEED_MIN) return
+  return round(35.74 + (0.6215 * temp) - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16))
+}
+
+export default {
+  us,
+  si,
+  bounds: {
+    SI_SPEED_MIN,
+    SI_TEMP_MAX,
+    US_SPEED_MIN,
+    US_TEMP_MAX
+  }
 }
--- test/windchillTest.js
+++ test/windchillTest.js
@@ -3,7 +3,24 @@ import windchill from '../src/windchill'
 
 describe('windchill', () => {
   it('calculates the windchill from windspeed and temp (F)', () => {
-    expect(windchill(40, 10), 'to equal', 34)
-    expect(windchill(20, 35), 'to equal', 0)
+    expect(windchill.us(40, 10), 'to equal', 34)
+    expect(windchill.us(20, 35), 'to equal', 0)
+  })
+
+  it('does not calculate for out of bounds numbers', function () {
+    expect(windchill.us(51, 0), 'to equal', undefined)
+  })
+
+  it('supports SI units', function () {
+    expect(windchill.si(10, 20), 'to equal', 7)
+  })
+
+  it('returns bounds used in formulas', function () {
+    expect(windchill.bounds, 'to have keys', [
+      'SI_SPEED_MIN',
+      'SI_TEMP_MAX',
+      'US_SPEED_MIN',
+      'US_TEMP_MAX'
+    ])
   })
 })
