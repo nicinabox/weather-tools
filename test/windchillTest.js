00ccf8726c03a5efd4da300506d92dfe9351ae0b
Nic Aitch <nic@nicinabox.com>
2016-11-10 12:31:24 -0600

    Update test

--- test/windchillTest.js
+++ test/windchillTest.js
@@ -7,12 +7,17 @@ describe('windchill', () => {
     expect(windchill.us(20, 35), 'to equal', 0)
   })
 
-  it('does not calculate for out of bounds numbers', function () {
+  it('does not calculate for out of bounds US numbers', function () {
     expect(windchill.us(51, 0), 'to equal', undefined)
   })
 
   it('supports SI units', function () {
     expect(windchill.si(10, 20), 'to equal', 7)
+    expect(windchill.si(2.2, 6.4), 'to equal', 0)
+  })
+
+  it('does not calculate for out of bounds SI numbers', function () {
+    expect(windchill.si(11, 4), 'to equal', undefined)
   })
 
   it('returns bounds used in formulas', function () {
